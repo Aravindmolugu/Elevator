90fe6b426ad4ac4b2d6503db9d1c97fc1f4191a0
lenovo <lenovo@DESKTOP-F7O5BKL>
2018-09-08 15:01:01 +0530

    commit1

---
+++ Elevator/src/Elevator.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.Math;
+
+public class Elevator
+{
+
+    static int floor=0,destination,person = 0;
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        floor=((int) (Math.random() * 10 + 1));
+        System.out.println("The elevator is now on floor " +floor);
+        System.out.print("Which floor are you at now (0-10): ");
+        destination= s.nextInt();
+        if(floor == destination)
+        {
+            System.out.println("Enter the elevator");
+        }
+        else if(floor > destination)
+        {
+            ElevatorDown();
+        }
+        else if(floor < destination)
+        {
+            ElevatorUp();
+        }
+        System.out.println("To which floor would you want to go (0-10) where 0 = basement");
+        destination = s.nextInt();
+        if(floor > destination)
+        {
+            ElevatorDown();
+        }
+        else if(floor < destination)
+        {
+            ElevatorUp();
+        }
+    }
+    public static void ElevatorUp()
+    {
+        System.out.println("The elevator is on it's way up...");
+
+        for (person = destination; destination>=floor; floor++)
+
+            System.out.println("You are on"+floor);
+
+        System.out.println("The elevator has reached to "+destination+"floor");
+    }
+
+    public static void ElevatorDown()
+    {
+        System.out.println("The elevator is on it's way down...");
+        for (person = destination; destination<=floor; floor--)
+
+            System.out.println("You are on "+floor);
+
+        System.out.println("The elevator has reached to "+destination);
+        if(floor!=1&floor!=2&floor!=3)
+        	System.out.println("th floor");
+    }
+}
+
